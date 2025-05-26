3b47974fd13e8ae8761366e2b44d4e28db128100
GoDuHwan <rhenghks5167@naver.com>
2025-05-27 00:28:01 +0900

    Refactor: transactionDaoImpl에 insert를 삽입 즉시 account에 적용되게 수정 및 accout 테이블 수정

--- src/main/java/account/dao/AccountDao.java
+++ src/main/java/account/dao/AccountDao.java
@@ -2,12 +2,15 @@ package account.dao;
 
 import account.domain.AccountVO;
 
+import java.sql.Connection;
 import java.util.List;
 
 public interface AccountDao {
     int insertAccount(AccountVO account);              // 계좌 등록
-    AccountVO getAccountById(int accountId);        // 계좌 ID로 조회
     AccountVO getAccountByUserId(int userId); // 유저 ID로 조회
-    int updateBalance(int accountId, double balance);  // 잔액 업데이트
+    AccountVO getAccountById(Connection conn, int accountId);    // 계좌 ID로 조회
+
+    int updateBalance(Connection conn, int accountId, double balance);
+
     int deleteAccount(int accountId);         // 계좌 삭제
 }
