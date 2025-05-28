d71fa725b37f11d834ed3593aef52a8b0e1b2b64
SooHyeon Lee <dalsu4594@naver.com>
2025-05-28 13:08:28 +0900

    Refactor : 계좌 형식 변경

--- src/main/java/util/AccountUtil.java
+++ src/main/java/util/AccountUtil.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class AccountUtil {
     public static String generateAccountNumber() {
-        long timestamp = System.currentTimeMillis() % 10000000000L;  // 10자리
-        int random =new Random().nextInt(9000)+1000;
-        return String.format("%010d%4d", timestamp, random);
+        long timestamp = System.currentTimeMillis() % 1_000_000L;  // 앞 6자리
+        int random = new Random().nextInt(1_000_000);              // 뒤 6자리
+        return String.format("%06d-%06d", timestamp, random);
     }
 }
