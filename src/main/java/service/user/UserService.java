ebdd8549c350d41856778085d92943f1813ea810
SooHyeon Lee <dalsu4594@naver.com>
2025-05-28 13:09:44 +0900

    Feat : getAccountByUserId 추가(계좌, 화면 출력), regitser 계좌 중복 생성 때 재생성

--- src/main/java/service/user/UserService.java
+++ src/main/java/service/user/UserService.java
@@ -1,6 +1,10 @@
 package service.user;
 
+import account.dao.AccountDao;
+import account.domain.AccountVO;
+
 public interface UserService {
     boolean register(String name, String loginId, String password, String phone, String address, String ssn);
     boolean login(String loginId, String password);
+    AccountVO getAccountByUserId(int userId);
 }
